4cdde45f2f280d4d999cb4b248755aff389ed613
Katarzyna Duchowska <k.duchowska@o2.pl>
2023-02-09 19:19:02 +0100

    fixes

--- src/main/java/com/logintegra/wsbbugtracker/audit/AuditDataMapper.java
+++ src/main/java/com/logintegra/wsbbugtracker/audit/AuditDataMapper.java
@@ -1,16 +1,11 @@
 package com.logintegra.wsbbugtracker.audit;
 
-import lombok.experimental.UtilityClass;
-import org.springframework.stereotype.Component;
-
 import java.util.List;
 import java.util.stream.Collectors;
 
-@Component
-@UtilityClass
 public class AuditDataMapper {
 
-    public List<AuditDataDTO> map(List<Object[]> rawRevisions) {
+    public static List<AuditDataDTO> map(List<Object[]> rawRevisions) {
         return rawRevisions.stream()
                 .map(r -> new AuditDataDTO((Object[]) r))
                 .collect(Collectors.toList());
